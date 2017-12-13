00e16f8298452411cca82fa63af39964815468b9
DESKTOP-N596UES\Dasha <dashazheltovskikh@gmail.com>
2017-12-14 00:39:51 +0500

    DZ PNRPU game // картинка начального экрана

---
+++ app/src/main/java/pnrpu/bestgameever/ru/pnrpu/MainActivity.java
@@ -0,0 +1,13 @@
+package pnrpu.bestgameever.ru.pnrpu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
