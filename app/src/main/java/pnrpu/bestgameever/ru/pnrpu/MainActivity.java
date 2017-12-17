a218521e4117c6ebe2d4a5e1b079d2a55069a475
DESKTOP-N596UES\Dasha <dashazheltovskikh@gmail.com>
2017-12-17 18:44:15 +0500

    DZ PNRPU // главный экран, рабочая кнопка

--- app/src/main/java/pnrpu/bestgameever/ru/pnrpu/MainActivity.java
+++ app/src/main/java/pnrpu/bestgameever/ru/pnrpu/MainActivity.java
@@ -2,6 +2,8 @@ package pnrpu.bestgameever.ru.pnrpu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button startButton = (Button)findViewById(R.id.menuPNRPUbut);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // while (startButton.isPressed() == true) {
+                    startButton.setBackgroundResource(R.drawable.button_press);
+              //  }
+            }
+        });
     }
 }
